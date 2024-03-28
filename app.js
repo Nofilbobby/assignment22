47ad0daff0f0a2b993f5df252c3c774532794708
nofil <gnick0137@gmail.com>
2024-03-28 07:12:36 -0700

    frist commit

---
+++ app.js
@@ -0,0 +1,116 @@
+//Q no 1
+
+
+//  var number = prompt("Enter a positive integer");
+// var nubber =Math.round(number);
+// var cabber =Math.ceil(number);
+// var fobber =Math.floor(number)
+
+// document.write("Number:",number,"<br>"," Round off value:",nubber,"<br>","Floor value:",fobber,"<br>","ceil value:",cabber)
+
+//Q no 2
+
+
+// var number123 = prompt("Enter a negative integer");
+// var nubber123 =Math.round(number123);
+// var cabber123 =Math.ceil(number123);
+// var fobber123 =Math.floor(number123)
+
+// document.write("Number:",number123,"<br>"," Round off value:",nubber123,"<br>","Floor value:",fobber123,"<br>","ceil value:",cabber123)
+
+
+
+
+// Q no 3
+
+// var num = prompt("Enter a number:");
+
+//   document.write(Math.abs(num));
+
+// Q no 4
+// var num = Math.floor(Math.random() * 6) + 1;
+
+//  document.write("random dice value:",num);
+
+
+// Q no 5
+
+//  var headUser = prompt("Enter your name");
+// var tailUser = prompt("Enter your name");
+
+// var toss = Math.random() * 2;
+
+// var convert = Math.floor(toss);
+
+// if (convert === 0) {
+//       document.write(headUser, " win the toss");
+//     } else {
+//           document.write(tailUser, " win the toss");
+//         }
+        
+        
+        
+        
+// Q no 6
+
+// var ran =Math.floor( Math.random() * 100)+1;
+
+
+// document.write(ran)
+
+// Q no 7
+
+
+
+
+
+// var userWeight = prompt("Please enter your weight:");
+
+
+// var weight = parseFloat(userWeight);
+// var unit = "kgs";
+
+// if (userWeight.endsWith("kilograms") || userWeight.endsWith("kilogram")) {
+  
+//   userWeight = userWeight.substring(0, userWeight.length - 2);
+//   weight = parseFloat(userWeight);
+//   unit = "kilograms";
+// } else if (userWeight.endsWith("kg") || userWeight.endsWith("g") || userWeight.endsWith("gram")) {
+
+//   userWeight = userWeight.substring(0, userWeight.length - 2);
+//   weight = parseFloat(userWeight) / 1000;
+//   unit = "kilograms";
+// }
+
+// document.write("Your weight is: " + weight + " " + unit);
+
+
+// Q no 8
+
+//  var rango =Math.floor( Math.random() * 10) +1;
+
+// var luckey = prompt("Entera number between 1 and 10");
+// if(luckey===rango){
+//   document.write( "congratulation you won"  )  
+// }
+// else{
+//    document.write( "Try again!")
+// }
+
+
+
+
+//Next assignment
+
+// Q no 1
+
+// var num = 123.590
+
+//  document.write(Math.round(num))
+
+
+// Q no 2
+
+
+
+
